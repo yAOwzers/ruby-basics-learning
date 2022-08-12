dff9077755f47231b4e8bf5570e28696249a6869
J Neo <joel@kisi.io>
2022-08-12 12:19:01 +0200

    Ruby Arrays

---
+++ Arrays.rb
@@ -0,0 +1,17 @@
+days_of_week = Array[ "Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun" ]
+
+days_of_week.empty?
+
+puts days_of_week.at(0)
+
+puts days_of_week[1]
+
+puts days_of_week[-2]
+
+puts days_of_week.first
+
+puts days_of_week.last
+
+puts days_of_week[1..3]
+puts days_of_week[1,3]
+puts days_of_week.slice(1..3)
